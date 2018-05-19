ba67ea64e4071745457a5d5a2afca35542fddb86
ArnoldKermeci <k.arnold111@gmail.com>
2018-05-19 21:01:12 +0200

    Initial commit

---
+++ app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
@@ -0,0 +1,52 @@
+package rs.aleph.android.example12.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import rs.aleph.android.example12.R;
+
+public class ThirdActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onCreate()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onStart() {
+        super.onStart();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onStart()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onRestart()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onResume() {
+        super.onResume();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onResume()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onPause() {
+        super.onPause();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onPause()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onStop() {
+        super.onStop();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onStop()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onDestroy()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
